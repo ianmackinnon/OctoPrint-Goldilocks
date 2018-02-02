0a55003f249115f01b4b5acb2b5928141355bc1a
Ian Mackinnon <imackinnon@gmail.com>
2018-02-02 19:11:51 +0100

    Added G-Code send and receive hooks.

--- octoprint_goldilocks/__init__.py
+++ octoprint_goldilocks/__init__.py
@@ -1,72 +1,61 @@
 # coding=utf-8
 from __future__ import absolute_import
 
-### (Don't forget to remove me)
-# This is a basic skeleton for your plugin's __init__.py. You probably want to adjust the class name of your plugin
-# as well as the plugin mixins it's subclassing from. This is really just a basic skeleton to get you started,
-# defining your plugin as a template plugin, settings and asset plugin. Feel free to add or remove mixins
-# as necessary.
-#
-# Take a look at the documentation on what other plugin mixins are available.
-
 import octoprint.plugin
 
-class GoldilocksPlugin(octoprint.plugin.SettingsPlugin,
-                       octoprint.plugin.AssetPlugin,
-                       octoprint.plugin.TemplatePlugin):
-
-	##~~ SettingsPlugin mixin
-
-	def get_settings_defaults(self):
-		return dict(
-			# put your plugin's default settings here
-		)
-
-	##~~ AssetPlugin mixin
-
-	def get_assets(self):
-		# Define your plugin's asset files to automatically include in the
-		# core UI here.
-		return dict(
-			js=["js/goldilocks.js"],
-			css=["css/goldilocks.css"],
-			less=["less/goldilocks.less"]
-		)
-
-	##~~ Softwareupdate hook
-
-	def get_update_information(self):
-		# Define the configuration for your plugin to use with the Software Update
-		# Plugin here. See https://github.com/foosel/OctoPrint/wiki/Plugin:-Software-Update
-		# for details.
-		return dict(
-			goldilocks=dict(
-				displayName="Goldilocks Plugin",
-				displayVersion=self._plugin_version,
-
-				# version check: github repository
-				type="github_release",
-				user="ianmackinnon",
-				repo="OctoPrint-Goldilocks",
-				current=self._plugin_version,
-
-				# update method: pip
-				pip="https://github.com/ianmackinnon/OctoPrint-Goldilocks/archive/{target_version}.zip"
-			)
-		)
+__plugin_name__ = "Goldilocks"
 
 
-# If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
-# ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
-# can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
-__plugin_name__ = "Goldilocks Plugin"
+
+class GoldilocksPlugin(octoprint.plugin.StartupPlugin):
+    def on_after_startup(self):
+        self._logger.info("Goldilocks")
+
+
+    def get_update(self):
+        return dict(
+            goldilocks=dict(
+                displayName="Goldilocks",
+                displayVersion=self._plugin_version,
+
+                type="github_release",
+                user="ianmackinnon",
+                repo="OctoPrint-Goldilocks",
+                current=self._plugin_version,
+
+                pip="https://github.com/ianmackinnon/OctoPrint-Goldilocks/archive/{target_version}.zip"
+            )
+        )
+
+    def action_handler(self, _comm, _line, action, *args, **kwargs):
+        self._logger.info("Goldilocks received \"%s\" action from printer", action)
+
+    def gcode_rewrite_q(self, comm_instance, phase, cmd, cmd_type, gcode, *args, **kwargs):
+        self._logger.info("Goldilocks Q \"%s\"", cmd)
+        return cmd
+
+    def gcode_rewrite_s(self, comm_instance, phase, cmd, cmd_type, gcode, *args, **kwargs):
+        self._logger.info("Goldilocks S \"%s\"", cmd)
+        return cmd
+
+    def gcode_receive(self, comm, line, *args, **kwargs):
+        self._logger.info("Goldilocks R \"%s\"", line)
+        return line
+
+
+
 
 def __plugin_load__():
-	global __plugin_implementation__
-	__plugin_implementation__ = GoldilocksPlugin()
+    global __plugin_implementation__
+    global __plugin_hooks__
 
-	global __plugin_hooks__
-	__plugin_hooks__ = {
-		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
-	}
+    plugin = GoldilocksPlugin()
 
+    __plugin_implementation__ = plugin
+    __plugin_hooks__ = {
+        "octoprint.plugin.softwareupdate.check_config": plugin.get_update,
+        "octoprint.comm.protocol.action": plugin.action_handler,
+        "octoprint.comm.protocol.gcode.queuing": plugin.gcode_rewrite_q,
+        "octoprint.comm.protocol.gcode.sending": plugin.gcode_rewrite_s,
+        "octoprint.comm.protocol.gcode.received": plugin.gcode_receive,
+    }
